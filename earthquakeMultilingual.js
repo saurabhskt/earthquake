74e58fa687814487874115bd22bb6f5a5dd68f8a
Saurabh Kumar Tiwari <saurabhtiwari207@gmail.com>
2024-09-24 12:16:16 +0530

    Update earthquakeMultilingual.js

--- earthquakeMultilingual.js
+++ earthquakeMultilingual.js
@@ -1,35 +1,62 @@
 (function () {
     var myConnector = tableau.makeConnector();
     var columnNames = [];
-
-    myConnector.getSchema = function (schemaCallback) {
-        var cols = columnNames.map(function(colName) {
-            return {
-                id: colName,
-                dataType: inferDataType(colName)
-            };
-        });
-
+function getColumns() {
+        return [
+            { id: "plan1yearRangeId", dataType: tableau.dataTypeEnum.int },
+            { id: "plan1yearRangeName", dataType: tableau.dataTypeEnum.string },
+            { id: "season01", dataType: tableau.dataTypeEnum.string },
+            { id: "season02", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth01", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth02", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth03", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth04", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth05", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth06", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth07", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth08", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth09", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth10", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth11", dataType: tableau.dataTypeEnum.string },
+            { id: "yearMonth12", dataType: tableau.dataTypeEnum.string },
+            { id: "plan1yearGrouping", dataType: tableau.dataTypeEnum.string },
+            { id: "gpsTypology", dataType: tableau.dataTypeEnum.string },
+            { id: "countryCode", dataType: tableau.dataTypeEnum.string },
+            { id: "currencyCode", dataType: tableau.dataTypeEnum.string },
+            { id: "countryCurrency", dataType: tableau.dataTypeEnum.string },
+            { id: "supplierCode", dataType: tableau.dataTypeEnum.string },
+            { id: "supplierName", dataType: tableau.dataTypeEnum.string },
+            { id: "season01SupplierAllocQtyUnit", dataType: tableau.dataTypeEnum.float },
+            { id: "season01SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "season02SupplierAllocQtyUnit", dataType: tableau.dataTypeEnum.float },
+            { id: "season02SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month01SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month02SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month03SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month04SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month05SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month06SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month07SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month08SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month09SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month10SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month11SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float },
+            { id: "month12SupplierAllocQtyPcs", dataType: tableau.dataTypeEnum.float }
+        ];
+    }
+  myConnector.getSchema = function (schemaCallback) {
+        var cols = getColumns();
         var tableSchema = {
             id: "kiaibiAPIData",
             alias: "Kiabi API Data",
             columns: cols
         };
-
         schemaCallback([tableSchema]);
     };
 
-    function inferDataType(colName) {
-        if (colName.includes("Id") || colName.endsWith("Pcs") || colName.endsWith("Unit")) {
-            return tableau.dataTypeEnum.float;
-        } else if (colName.startsWith("year") || colName.startsWith("month") || colName.includes("Name") || colName.includes("Code")) {
-            return tableau.dataTypeEnum.string;
-        } else {
-            return tableau.dataTypeEnum.string; // Default to string for unknown types
-        }
-    }
+  
 
-    myConnector.getData = function(table, doneCallback) {
+ myConnector.getData = function(table, doneCallback) {
         var connectionData = JSON.parse(tableau.connectionData);
         var apiToken = tableau.password;
         var oneYearId = connectionData.oneYearId;
@@ -53,15 +80,10 @@
                 var feat = resp.result,
                     tableData = [];
 
-                // If columnNames is empty, populate it with the keys from the first result
-                if (columnNames.length === 0 && feat.length > 0) {
-                    columnNames = Object.keys(feat[0]);
-                }
-
                 for (var i = 0, len = feat.length; i < len; i++) {
                     var row = {};
-                    columnNames.forEach(function(colName) {
-                        row[colName] = feat[i][colName];
+                    getColumns().forEach(function(col) {
+                        row[col.id] = feat[i][col.id];
                     });
                     tableData.push(row);
                 }
